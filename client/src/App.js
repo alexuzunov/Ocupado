95a5ce1892091b83cd3130fee483d67b6bce49ed
Antoniya Hristova <karma12451@gmail.com>
2023-12-20 18:48:43 +0200

    Created homepage file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Homepage from "./components/Homepage";
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
+        <Route path="/homepage" element={<Homepage/>}/>
       </Routes>
     </BrowserRouter>
   );
